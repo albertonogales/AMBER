42fd7c937ac33dbb6ec30d39c2664ca197f6fc0f
Alberto Nogales <alberto.nogales@uah.es>
2026-07-18 13:32:21 +0200

    fix: wrap long import in map.py to satisfy ruff line-length (I001)

--- AMBER/map.py
+++ AMBER/map.py
@@ -8,7 +8,12 @@ from typing import Optional, Tuple
 import numpy as np
 from tqdm.auto import tqdm
 
-from .distances import AVAILABLE_DISTANCES, GRID_DISTANCE, SIGNAL_DISTANCE_MATRIX, _DTW_WARN_THRESHOLD
+from .distances import (
+    AVAILABLE_DISTANCES,
+    GRID_DISTANCE,
+    SIGNAL_DISTANCE_MATRIX,
+    _DTW_WARN_THRESHOLD,
+)
 
 logger = logging.getLogger(__name__)
 
